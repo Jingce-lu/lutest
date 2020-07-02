0bf6f7f9172834d8d764f839e8feb9c5637085e1
Jingce-lu <lujingce@163.com>
2020-07-02 15:38:13 +0800

    ci rest

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-set -e
+set -
+
+REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+BRANCH="gh-pages"
 
 # 生成静态文件
 npm run build
@@ -9,18 +12,27 @@ npm run build
 # 进入生成的文件夹
 cd dist
 
+# ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'init'
+# git init
+# git add -A
+# git commit -m 'init'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Jingce-lu/lutest.git master:gh-pages
+# git push -f git@github.com:Jingce-lu/lutest.git master:gh-pages
 
+# cd -
+# ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
-cd -
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+git commit -m "published by GitHub Actions"
+git push --force ${REPO} master:${BRANCH}
+
